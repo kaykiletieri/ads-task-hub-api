57e482d3db82396e7d06d8e67f743869ebb3d9bf
Kayki Letieri <kaykiletieri37@gmail.com>
2025-04-26 22:22:44 -0300

    feat: add modules for users, tasks, classes, and periods with controllers and services
    - Implemented UsersModule with UsersController and UsersService
    - Implemented TasksModule with TasksController and TasksService
    - Implemented ClassesModule with ClassesController and ClassesService
    - Implemented PeriodsModule with PeriodsController and PeriodsService
    - Added TypeORM configuration in app.module.ts for PostgreSQL
    - Created unit tests for all controllers and services

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,29 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { UsersModule } from './modules/users/users.module';
+import { TasksModule } from './modules/tasks/tasks.module';
+import { ClassesModule } from './modules/classes/classes.module';
+import { PeriodsModule } from './modules/periods/periods.module';
 
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: process.env.DB_HOST,
+      port: +(process.env.DB_PORT || 5432),
+      username: process.env.DB_USER,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_NAME,
+      entities: [],
+      synchronize: true,
+    }),
+    UsersModule,
+    TasksModule,
+    ClassesModule,
+    PeriodsModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
